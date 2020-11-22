a0116364d2ca0c564d0ba7cf6837b6af9bc0fbdf
Ramonacus <ramonacus@gmail.com>
2020-11-22 23:44:59 +0100

    Move NPC to a separate class, as it was getting bigger

--- sample/main.py
+++ sample/main.py
@@ -1,37 +1,4 @@
-import datetime
-import random
-from attributes import gender, sexuality, name
-
-
-campaignDate = datetime.date(1920, 12, 31)
-
-
-class NPC:
-    """ Procedurally generates a NPC.
-    Should add config to everything in the future. """
-
-    def __init__(self):
-        # Define sexual traits and name
-        self.gender = gender.Gender()
-        self.sexuality = sexuality.Sexuality()
-        self.name = name.generate(self.gender.name)
-
-        # Define age in relationship with campaign setting
-        currentDate = campaignDate or datetime.date.today()
-        ageRange = random.randrange(365.25 * 80)
-        self.birthday = currentDate - datetime.timedelta(days=ageRange)
-
-    @property
-    def age(self):
-        dob = self.birthday
-        current = campaignDate or datetime.date.today()
-        years = current.year - dob.year
-        if (current.month < dob.month or
-            current.month == dob.month and
-            current.day < dob.day):
-            # The date of birth is later during the year, decrease the age by 1
-            years -= 1
-        return years
+from npc import NPC
 
 
 npc = NPC()
---
+++ sample/npc.py
@@ -0,0 +1,35 @@
+import datetime
+import random
+from attributes import gender, sexuality, name
+
+
+# TODO fix this quirk. Create class Campaign? World?
+campaignDate = datetime.date(1920, 12, 31)
+
+
+class NPC:
+    """ Procedurally generates a NPC.
+    Should add config to everything in the future. """
+
+    def __init__(self):
+        # Define sexual traits and name
+        self.gender = gender.Gender()
+        self.sexuality = sexuality.Sexuality()
+        self.name = name.generate(self.gender.name)
+
+        # Define age in relationship with campaign setting
+        currentDate = campaignDate or datetime.date.today()
+        ageRange = random.randrange(365.25 * 80)
+        self.birthday = currentDate - datetime.timedelta(days=ageRange)
+
+    @property
+    def age(self):
+        dob = self.birthday
+        current = campaignDate or datetime.date.today()
+        years = current.year - dob.year
+        if (current.month < dob.month or
+            current.month == dob.month and
+            current.day < dob.day):
+            # The date of birth is later during the year, decrease the age by 1
+            years -= 1
+        return years

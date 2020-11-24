5f932a9de20b0b5792a425b03addbe192ce05403
Ramonacus <ramonacus@gmail.com>
2020-11-24 21:30:07 +0100

    Better grouping of beliefs in description

--- sample/main.py
+++ sample/main.py
@@ -1,10 +1,19 @@
 from npc import NPC
+from itertools import groupby
 
 
 npc = NPC()
 
 print(f"{npc.name.full()} is a {npc.sexuality.name} {npc.gender.name}")
-print(f"{npc.gender.pronoun.capitalize()} was born on {npc.birthday.strftime('%b %d %Y')}")
+print(f"{npc.gender.pronoun.capitalize()} was born on\
+    {npc.birthday.strftime('%b %d %Y')}")
 print(f"{npc.gender.pronoun.capitalize()} is now {npc.age} years old")
-for belief in npc.beliefSystem.beliefs:
-    print(f"{belief.name} value is {belief.value}")
+
+def groupBeliefs(group):
+    regroup = list(belief.name for belief in group)
+    return regroup[0] if len(regroup) == 1\
+        else ' and '.join([', '.join(regroup[:-1]), regroup[-1]])
+
+print(f"{npc.gender.pronoun.capitalize()} has the following belief levels:")
+for key, group in groupby(npc.beliefSystem.beliefs, lambda x: x.value):
+    print(f"* {key} for {groupBeliefs(group)}")
--- sample/npc.py
+++ sample/npc.py
@@ -6,7 +6,7 @@ import beliefs
 
 # TODO fix this quirk. Create class Campaign? World?
 campaignDate = datetime.date(1934, 12, 31)
-
+currentDate = campaignDate or datetime.date.today()
 
 class NPC:
     """ Procedurally generates a NPC.
@@ -19,21 +19,21 @@ class NPC:
         self.name = attributes.generate_name(self.gender.name)
 
         # Define age in relationship with campaign setting
-        currentDate = campaignDate or datetime.date.today()
         ageRange = random.randrange(365.25 * 80)
         self.birthday = currentDate - datetime.timedelta(days=ageRange)
 
         # Belief system
         self.beliefSystem = beliefs.BeliefSystem()
 
+    # Get the age for the current campaignDate
     @property
     def age(self):
         dob = self.birthday
         current = campaignDate or datetime.date.today()
         years = current.year - dob.year
-        if (current.month < dob.month or
-            current.month == dob.month and
-            current.day < dob.day):
+        if (current.month < dob.month
+            or current.month == dob.month
+            and current.day < dob.day):
             # The date of birth is later during the year, decrease the age by 1
             years -= 1
         return years
